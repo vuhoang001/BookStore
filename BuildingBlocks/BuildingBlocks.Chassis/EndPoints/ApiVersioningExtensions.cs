233baca86185af9b72ce0092a362283944c52284
vuhoang001 <vuhoang250203@gmail.com>
2026-02-25 17:09:21 +0700

    feat: config endpoint

---
+++ BuildingBlocks/BuildingBlocks.Chassis/EndPoints/ApiVersioningExtensions.cs
@@ -0,0 +1,23 @@
+using Asp.Versioning;
+using BuildingBlocks.Constants.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Chassis.EndPoints;
+
+public static class Extension
+{
+    public static void AddVersioning(this IServiceCollection service)
+    {
+        service
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = Versions.V1;
+                options.ApiVersionReader  = new UrlSegmentApiVersionReader();
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat           = "'v'V";
+                options.SubstituteApiVersionInUrl = true;
+            });
+    }
+}
---
+++ BuildingBlocks/BuildingBlocks.Constants/Core/ApiVersions.cs
@@ -0,0 +1,6 @@
+namespace BuildingBlocks.Constants.Core;
+
+public class ApiVersions
+{
+    
+}
